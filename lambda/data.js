4389f03c7cbf68340d49eae3888cdf7678b0a27b
Marcel Fahle <m.fahle@gmail.com>
2017-01-08 11:44:53 +0100

    adds core funcionality to grab an MP3 from somewhere nd get it\'s duration

--- lambda/data.js
+++ lambda/data.js
@@ -1,3 +1,3 @@
 module.exports = {
-  file: 'speech.mp3'
+  file: 'https://s3-eu-west-1.amazonaws.com/audicle-storage/development/posts/bf62089097033ad55de9295fc5b1a3ab.mp3'
 }
--- lambda/index.js
+++ lambda/index.js
@@ -1,12 +1,25 @@
-
+var fs = require('fs');
+var request = require('request');
 var mp3Duration = require('mp3-duration');
 
 // get mp3 duration from S3.
  
 exports.handler = (event, context, callback) => {
-  mp3Duration(event.file, function (err, duration) {
-    if (err) throw (err);
-    callback(null, {duration});
-  });
+  request
+    .get(event.file)
+    .on('error', (err) => {
+      throw (err);
+    })
+    .pipe(fs.createWriteStream('audio.mp3'))
+    .on('error', (err) => {
+      throw (err);
+    })
+    .on('finish', () => {
+      mp3Duration('audio.mp3', (err, duration) => {
+        if (err) throw (err);
+        fs.unlink('audio.mp3');
+        callback(null, {duration});
+      });
+    });
 };
 
